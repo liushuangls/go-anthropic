cdb5625d22b5dd7a253c37be1f3474ee01933058
liushuang <liushuangbill@outlook.com>
2024-03-05 09:45:55 +0800

    feat: add claude-3-opus-20240229  and claude-3-sonnet-20240229 models

--- common.go
+++ common.go
@@ -1,9 +1,11 @@
 package anthropic
 
 const (
-	ModelClaudeInstant1Dot2 = "claude-instant-1.2"
-	ModelClaude2Dot0        = "claude-2.0"
-	ModelClaude2Dot1        = "claude-2.1"
+	ModelClaudeInstant1Dot2    = "claude-instant-1.2"
+	ModelClaude2Dot0           = "claude-2.0"
+	ModelClaude2Dot1           = "claude-2.1"
+	ModelClaude3Opus20240229   = "claude-3-opus-20240229"
+	ModelClaude3Sonnet20240229 = "claude-3-sonnet-20240229"
 )
 
 const (
