492fee446479c81f380ddb0f2105d85130626a88
liushuang <liushuangbill@outlook.com>
2024-03-14 10:12:37 +0800

    feat: add claude-3-haiku-20240307 model

--- common.go
+++ common.go
@@ -6,6 +6,7 @@ const (
 	ModelClaude2Dot1           = "claude-2.1"
 	ModelClaude3Opus20240229   = "claude-3-opus-20240229"
 	ModelClaude3Sonnet20240229 = "claude-3-sonnet-20240229"
+	ModelClaude3Haiku20240307  = "claude-3-haiku-20240307"
 )
 
 const (
