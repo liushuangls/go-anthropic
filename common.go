60c42fe9cd6b6dd18066e8326c8a9606c2366d0c
liushuang <liushuangbill@outlook.com>
2024-06-21 10:06:35 +0800

    feat: add ModelClaude3Sonnet20240229

--- common.go
+++ common.go
@@ -1,12 +1,13 @@
 package anthropic
 
 const (
-	ModelClaudeInstant1Dot2    = "claude-instant-1.2"
-	ModelClaude2Dot0           = "claude-2.0"
-	ModelClaude2Dot1           = "claude-2.1"
-	ModelClaude3Opus20240229   = "claude-3-opus-20240229"
-	ModelClaude3Sonnet20240229 = "claude-3-sonnet-20240229"
-	ModelClaude3Haiku20240307  = "claude-3-haiku-20240307"
+	ModelClaudeInstant1Dot2        = "claude-instant-1.2"
+	ModelClaude2Dot0               = "claude-2.0"
+	ModelClaude2Dot1               = "claude-2.1"
+	ModelClaude3Opus20240229       = "claude-3-opus-20240229"
+	ModelClaude3Sonnet20240229     = "claude-3-sonnet-20240229"
+	ModelClaude3Dot5Sonnet20240620 = "claude-3-5-sonnet-20240620"
+	ModelClaude3Haiku20240307      = "claude-3-haiku-20240307"
 )
 
 const (
