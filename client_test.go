82f9ccf5d7c37767fe60d9b48f53c0695d2bbf37
William Matthews <will.a.matthews@me.com>
2024-09-02 11:31:09 +0000

    multi version test

--- client_test.go
+++ client_test.go
@@ -6,14 +6,29 @@ import (
 )
 
 func TestWithBetaVersion(t *testing.T) {
-	opt := withBetaVersion("fake-version")
-	request, err := http.NewRequest("GET", "http://example.com", nil)
-	if err != nil {
-		t.Fatalf("http.NewRequest error: %s", err)
-	}
-	opt(request)
+	t.Run("single beta version", func(t *testing.T) {
+		opt := withBetaVersion("fake-version")
+		request, err := http.NewRequest("GET", "http://example.com", nil)
+		if err != nil {
+			t.Fatalf("http.NewRequest error: %s", err)
+		}
+		opt(request)
 
-	if req := request.Header.Get("anthropic-beta"); req != "fake-version" {
-		t.Errorf("unexpected BetaVersion: %s", req)
-	}
+		if req := request.Header.Get("anthropic-beta"); req != "fake-version" {
+			t.Errorf("unexpected BetaVersion: %s", req)
+		}
+	})
+
+	t.Run("multiple beta versions", func(t *testing.T) {
+		opt := withBetaVersion("fake-version1", "fake-version2")
+		request, err := http.NewRequest("GET", "http://example.com", nil)
+		if err != nil {
+			t.Fatalf("http.NewRequest error: %s", err)
+		}
+		opt(request)
+
+		if req := request.Header.Get("anthropic-beta"); req != "fake-version1,fake-version2" {
+			t.Errorf("unexpected BetaVersion: %s", req)
+		}
+	})
 }
