4060df80f6c09e6b395d13b7d4158d47ef865f47
Steve Heyman <steve@routeif.com>
2024-12-04 15:45:40 -0800

    Integration test for VertexAI

--- integrationtest/integration_test.go
+++ integrationtest/integration_test.go
@@ -14,3 +14,21 @@ func testAPIKey(t *testing.T) {
 		t.Fatal("ANTHROPIC_KEY must be set for integration tests")
 	}
 }
+
+var (
+	VertexAPIKey      = os.Getenv("VERTEX_KEY")
+	VertexAPILocation = os.Getenv("VERTEX_LOCATION")
+	VertexAPIProject  = os.Getenv("VERTEX_PROJECT")
+)
+
+func testVertexAPIKey(t *testing.T) {
+	if VertexAPIKey == "" {
+		t.Fatal("VERTEX_KEY must be set for integration tests")
+	}
+	if VertexAPILocation == "" {
+		t.Fatal("VERTEX_LOCATION must be set for integration tests")
+	}
+	if VertexAPIProject == "" {
+		t.Fatal("VERTEX_PROJECT must be set for integration tests")
+	}
+}
