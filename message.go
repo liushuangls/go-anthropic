86739f4b4c31d30d94aeb6624fb5ee96f6e2f34b
shadowpigy <shadowpigy@github.com>
2024-12-10 18:08:12 +0800

    feat: add util functions for setting Anthropic defined tools

--- message.go
+++ message.go
@@ -381,6 +381,30 @@ type ToolDefinition struct {
 	CacheControl *MessageCacheControl `json:"cache_control,omitempty"`
 }
 
+func NewComputerUseToolDefinition(name string, displayWidthPx int, displayHeightPx int, displayNumber *int) ToolDefinition {
+	return ToolDefinition{
+		Type:            "computer_20241022",
+		Name:            name,
+		DisplayWidthPx:  displayWidthPx,
+		DisplayHeightPx: displayHeightPx,
+		DisplayNumber:   displayNumber,
+	}
+}
+
+func NewTextEditorToolDefinition(name string) ToolDefinition {
+	return ToolDefinition{
+		Type: "text_editor_20241022",
+		Name: name,
+	}
+}
+
+func NewBashToolDefinition(name string) ToolDefinition {
+	return ToolDefinition{
+		Type: "bash_20241022",
+		Name: name,
+	}
+}
+
 type ToolChoice struct {
 	// oneof: auto(default) any tool
 	Type string `json:"type"`
