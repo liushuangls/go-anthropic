5e74c15ca17dcd996d1fcd2af5699aea08f990d0
mhpenta <cogito.huckelberry@gmail.com>
2025-10-03 13:06:00 -0600

    Add sonnet 4.5

--- common.go
+++ common.go
@@ -3,22 +3,24 @@ package anthropic
 type Model string
 
 const (
-	ModelClaude2Dot0               Model = "claude-2.0"
-	ModelClaude2Dot1               Model = "claude-2.1"
-	ModelClaude3Opus20240229       Model = "claude-3-opus-20240229"
-	ModelClaude3Sonnet20240229     Model = "claude-3-sonnet-20240229"
-	ModelClaude3Dot5Sonnet20240620 Model = "claude-3-5-sonnet-20240620"
-	ModelClaude3Dot5Sonnet20241022 Model = "claude-3-5-sonnet-20241022"
-	ModelClaude3Dot5SonnetLatest   Model = "claude-3-5-sonnet-latest"
-	ModelClaude3Haiku20240307      Model = "claude-3-haiku-20240307"
-	ModelClaude3Dot5HaikuLatest    Model = "claude-3-5-haiku-latest"
-	ModelClaude3Dot5Haiku20241022  Model = "claude-3-5-haiku-20241022"
-	ModelClaude3Dot7SonnetLatest   Model = "claude-3-7-sonnet-latest"
-	ModelClaude3Dot7Sonnet20250219 Model = "claude-3-7-sonnet-20250219"
-	ModelClaudeOpus4Dot0           Model = "claude-opus-4-0"
-	ModelClaudeOpus4V20250514      Model = "claude-opus-4-20250514"
-	ModelClaudeSonnet4Dot0         Model = "claude-sonnet-4-0"
-	ModelClaudeSonnet4V20250514    Model = "claude-sonnet-4-20250514"
+	ModelClaude2Dot0                Model = "claude-2.0"
+	ModelClaude2Dot1                Model = "claude-2.1"
+	ModelClaude3Opus20240229        Model = "claude-3-opus-20240229"
+	ModelClaude3Sonnet20240229      Model = "claude-3-sonnet-20240229"
+	ModelClaude3Dot5Sonnet20240620  Model = "claude-3-5-sonnet-20240620"
+	ModelClaude3Dot5Sonnet20241022  Model = "claude-3-5-sonnet-20241022"
+	ModelClaude3Dot5SonnetLatest    Model = "claude-3-5-sonnet-latest"
+	ModelClaude3Haiku20240307       Model = "claude-3-haiku-20240307"
+	ModelClaude3Dot5HaikuLatest     Model = "claude-3-5-haiku-latest"
+	ModelClaude3Dot5Haiku20241022   Model = "claude-3-5-haiku-20241022"
+	ModelClaude3Dot7SonnetLatest    Model = "claude-3-7-sonnet-latest"
+	ModelClaude3Dot7Sonnet20250219  Model = "claude-3-7-sonnet-20250219"
+	ModelClaudeOpus4Dot0            Model = "claude-opus-4-0"
+	ModelClaudeOpus4V20250514       Model = "claude-opus-4-20250514"
+	ModelClaudeSonnet4Dot0          Model = "claude-sonnet-4-0"
+	ModelClaudeSonnet4V20250514     Model = "claude-sonnet-4-20250514"
+	ModelClaudeSonnet4Dot5          Model = "claude-sonnet-4-5"
+	ModelClaudeSonnet4Dot5V20250929 Model = "claude-sonnet-4-5-20250929"
 )
 
 type ChatRole string
@@ -48,6 +50,8 @@ func (m Model) asVertexModel() string {
 		return "claude-opus-4@20250514"
 	case ModelClaudeSonnet4Dot0, ModelClaudeSonnet4V20250514:
 		return "claude-sonnet-4@20250514"
+	case ModelClaudeSonnet4Dot5, ModelClaudeSonnet4Dot5V20250929:
+		return "claude-sonnet-4-5@20250929"
 	default:
 		return string(m)
 	}
