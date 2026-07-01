cc70395a96d4268dc179d9f84c543f5c459513c7
Liu Shuang <liushuangbill@outlook.com>
2026-07-01 16:06:45 +0800

    fix: MessageContentThinking remove omitempty

--- message.go
+++ message.go
@@ -704,8 +704,8 @@ func (c *MessageContentToolUse) UnmarshalInput(v any) error {
 }
 
 type MessageContentThinking struct {
-	Thinking  string `json:"thinking,omitempty"`
-	Signature string `json:"signature,omitempty"`
+	Thinking  string `json:"thinking"`
+	Signature string `json:"signature"`
 }
 
 type MessageContentRedactedThinking struct {
